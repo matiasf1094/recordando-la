115f667fe08787b5b551bb203b01004d9c1e35f9
Matias Flores <matiasf1094@gmail.com>
2019-09-25 21:06:13 -0500

    Chinga tu madre XD

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,45 @@
+import { Component, Injectable } from '@angular/core';
+// Importamos la libreria de firebase database
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+	// Definimos las variables con su tipo de dato
+	mensaje : string = '';
+	listado : any = [];
+  recuerdos : any;
+
+  // Agregamos a nuestro codigo la libreria importada
+	constructor(
+   		//public afDB: AngularFireDatabase
+ 	){
+
+     /*afDB.object("recuerdos").valueChanges().subscribe(
+       (val) => {
+         console.log("resultados", val);
+       }
+     )*/
+
+  }
+
+
+
+  // Creamos una funcion que enviara los datos a firebase
+	guardar() {
+  		
+	}
+
+  asustar() {
+    
+    alert("Hola ");
+  }
+
+}
